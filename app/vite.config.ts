4bf3a5d06e052415a3f50daf44b2684b7cecde8f
Souren <drift10p0@gmail.com>
2025-06-25 17:10:17 -0400

    Update vite.config.ts

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -3,18 +3,17 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
-    react(),
-    // Removed `componentTagger()` because it's undefined and breaks the build
+    react()
   ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
